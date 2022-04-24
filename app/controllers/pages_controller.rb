2f9e7c99226008f8c754f550226a9b1e39dc8fe6
Paul Anthony McGowan <xhostcom@gmail.com>
2022-04-24 22:56:18 +0100

    Test Fail

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,24 @@
 # frozen_string_literal: true
-
+require 'faraday'
+require 'json'
+require 'faraday_middleware'
+HEADERS = {
+  'Content-Type': 'application/json',
+  'Accept': 'application/json',
+  'x-rapidapi-host': @host,
+  'x-rapidapi-key': ENV['RAPID_API_KEY']
+}.freeze
 class PagesController < ApplicationController
-  def fetch_data
-    @data = {
-      'title' => 'Fetch Data',
-      'description' => 'Fetch data from the database',
-      'data' => {
-        'users' => User.all,
-        'posts' => Post.all,
-        'comments' => Comment.all
-      }
-    }
+  def fetch_data(url, host, headers)
+    @url = url
+    @host = host
+    @headers = headers || HEADERS
+    if !@url.nil? && !@host.nil?
+      @response = Faraday.get(@url, headers: @headers)
+      @response_body = JSON.parse(@response.body)
+    else
+      Errno::ENOENT => 'Error: URL or Host not found'
+      @response_body = 'Please enter a valid URL'
+    end
   end
 end
--- spec/models/api_fetch_spec.rb
+++ spec/models/api_fetch_spec.rb
@@ -4,9 +4,10 @@ require 'rails_helper'
 
 RSpec.describe ApiFetch, type: :model do
   let(:api_fetch) { ApiFetch.new }
+  let(:fetch_data) { api_fetch.fetch_data }
   describe '#fetch_data' do
-    it 'returns a hash' do
-      expect(api_fetch.fetch_data).to be_a(Hash)
+    it 'makes a valid connection' do
+      expect(api_fetch.fetch_data).to be_truthy
     end
   end
 end
