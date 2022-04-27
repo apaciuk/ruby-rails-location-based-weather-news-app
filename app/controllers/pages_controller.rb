13c58113bba058aec95faa1f01378407141634e4
Paul Anthony McGowan <xhostcom@gmail.com>
2022-04-27 19:41:51 +0100

    Boilerplate QueryCreator class

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,47 +6,57 @@ require 'openssl'
 require 'json'
 class PagesController < ApplicationController
   def index
-    url = URI('https://latest-breaking-news-live.p.rapidapi.com/latest-breaking-news')
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    request = Net::HTTP::Get.new(url)
-    request['X-RapidAPI-Host'] = 'latest-breaking-news-live.p.rapidapi.com'
-    request['X-RapidAPI-Key'] = ENV['RAPID_API_KEY'] # 'd6170fbf28msh711604efe0f7970p1ce9f3jsnf68590abbea1'
-    response = http.request(request)
-    output = JSON.parse(response.read_body, symbolize_names: true)
-    # @news = output
-    render json: output
+    service = QueryCreator.run(index)
+    # service = User::SendNotification.run(user: User.first, provider: Provider.first)
+    if service.success?
+      render json: 'success'
+    else
+      render json: 'fail'
+    end
   end
 
-  def get_location
-    #  url = URI('https://ip-geo-location.p.rapidapi.com/ip/check?format=json')
-    # http = Net::HTTP.new(url.host, url.port)
-    # http.use_ssl = true
-    #  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    # request = Net::HTTP::Get.new(url)
-    #  request['X-RapidAPI-Host'] = 'ip-geo-location.p.rapidapi.com'
-    # request['X-RapidAPI-Key'] = 'd6170fbf28msh711604efe0f7970p1ce9f3jsnf68590abbea1'
-    # response = http.request(request)
-    #  output = JSON.parse(response.read_body, symbolize_names: true)
-    #  @location = output[:area][:name]
-  end
+  # def index_old
+  # url = URI('https://latest-breaking-news-live.p.rapidapi.com/latest-breaking-news')
+  #  http = Net::HTTP.new(url.host, url.port)
+  #  http.use_ssl = true
+  #  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  #  request = Net::HTTP::Get.new(url)
+  #  request['X-RapidAPI-Host'] = 'latest-breaking-news-live.p.rapidapi.com'
+  # request['X-RapidAPI-Key'] = ENV['RAPID_API_KEY'] # 'd6170fbf28msh711604efe0f7970p1ce9f3jsnf68590abbea1'
+  # response = http.request(request)
+  #  output = JSON.parse(response.read_body, symbolize_names: true)
+  # @news = output
+  #  render json: output
+  # end
 
-  def get_weather
-    # location = index.output[:area][:name]
-    # city = index.output[[:area][:name]]#{city}
-    #  url = URI('https://community-open-weather-map.p.rapidapi.com/weather?units=imperial&q=Rotherham')
-    #  http = Net::HTTP.new(url.host, url.port)
-    # http.use_ssl = true
-    # http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    # request = Net::HTTP::Get.new(url)
-    # request['X-RapidAPI-Host'] = 'community-open-weather-map.p.rapidapi.com'
-    # request['X-RapidAPI-Key'] = 'd6170fbf28msh711604efe0f7970p1ce9f3jsnf68590abbea1'
-    #  response = http.request(request)
-    # output = JSON.parse(response.read_body, symbolize_names: true)
-    # @output = output
-    #  @weather = output[:weather][0][:description]
-    # @icon = output[:weather][0][:icon]
-    # render json: @output
-  end
+  # def get_location
+  #  url = URI('https://ip-geo-location.p.rapidapi.com/ip/check?format=json')
+  # http = Net::HTTP.new(url.host, url.port)
+  # http.use_ssl = true
+  #  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  # request = Net::HTTP::Get.new(url)
+  #  request['X-RapidAPI-Host'] = 'ip-geo-location.p.rapidapi.com'
+  # request['X-RapidAPI-Key'] = 'd6170fbf28msh711604efe0f7970p1ce9f3jsnf68590abbea1'
+  # response = http.request(request)
+  #  output = JSON.parse(response.read_body, symbolize_names: true)
+  #  @location = output[:area][:name]
+  # end
+
+  # def get_weather
+  # location = index.output[:area][:name]
+  # city = index.output[[:area][:name]]#{city}
+  #  url = URI('https://community-open-weather-map.p.rapidapi.com/weather?units=imperial&q=Rotherham')
+  #  http = Net::HTTP.new(url.host, url.port)
+  # http.use_ssl = true
+  # http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  # request = Net::HTTP::Get.new(url)
+  # request['X-RapidAPI-Host'] = 'community-open-weather-map.p.rapidapi.com'
+  # request['X-RapidAPI-Key'] = 'd6170fbf28msh711604efe0f7970p1ce9f3jsnf68590abbea1'
+  #  response = http.request(request)
+  # output = JSON.parse(response.read_body, symbolize_names: true)
+  # @output = output
+  #  @weather = output[:weather][0][:description]
+  # @icon = output[:weather][0][:icon]
+  # render json: @output
+  # end
 end
--- app/services/query_creator.rb
+++ app/services/query_creator.rb
@@ -1,40 +1,77 @@
 # frozen_string_literal: true
+
 require 'uri'
 require 'net/http'
 require 'openssl'
 require 'json'
+require 'base64'
 class QueryCreator < ApplicationService
+  # Constants
+  X_RAPIDAPI_KEY = ENV['X_RAPIDAPI_KEY']
+  GEOLOCATION_URL = ENV['GEOLOCATION_URL'] || 'https://ip-geo-location.p.rapidapi.com/ip/check'
+  GEOLOCATION_HOST = ENV['GEOLOCATION_HOST'] || 'ip-geo-location.p.rapidapi.com'
+  WEATHER_URL = ENV['WEATHER_URL'] || 'https://community-open-weather-map.p.rapidapi.com/weather?units=imperial&q='
+  WEATHER_HOST = ENV['WEATHER_HOST'] || 'community-open-weather-map.p.rapidapi.com'
+  NEWS_URL = ENV['NEWS_URL'] || 'https://latest-breaking-news-live.p.rapidapi.com/latest-breaking-news'
+  NEWS_HOST = ENV['NEWS_HOST'] || 'latest-breaking-news-live.p.rapidapi.com'
+  attr_reader :step, :query, :location, :weather, :news
+
   # Arguments
-  arg :url, type: URI, required: true
-  arg :http, type: String
-  arg :request, type: String
+  arg :url, type: URI, required: true, context: true, desc: 'The URL to the query'
+  arg :http, type: Method, required: true, context: true, desc: 'The HTTP method to use'
+  arg :request, type: Object, required: true, context: true, desc: 'The request object'
+  arg :response, type: Object, required: true, context: true, desc: 'The response object'
 
   # Steps
-  step :create_query
-  step :create_query_result
-  step :create_query_result_item
+  step :index
+  step :create_clim_query, after: :index, input: :loc_output
+  step :create_news_query, after: :create_clim_query, input: :clim_output
 
   # Outputs
-  output :response
+  output :loc_output
+  output :clim_output
+  output :news_output
 
   private
 
-  def initialize(params)
-    super()
-    @params = params
+  def index
+    url = URI('GEOLOCATION_URL') || URI('https://ip-geo-location.p.rapidapi.com/ip/check?format=json')
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true if url.scheme == 'https' && !http.started?
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(url)
+    request['X-RapidAPI-Host'] = 'GEOLOCATION_HOST'
+    request['X-RapidAPI-Key'] = 'X_RAPIDAPI_KEY'
+    response = http.request(request)
+    loc_output = JSON.parse(response.read_body, symbolize_names: true)
+    render json: loc_output
+    run create_clim_query, loc_output:
   end
 
-  def loc_client
-    url = URI('https://ip-geo-location.p.rapidapi.com/ip/check?format=json')
-    loc_client = QueryCreator::Net::HTTP.new(url.host, url.port)
-    loc_client.use_ssl = true
-    loc_client.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    request = QueryCreator::Net::HTTP::Get.new(url)
-    request['X-RapidAPI-Host'] = 'ip-geo-location.p.rapidapi.com'
-    request['X-RapidAPI-Key'] = ENV['RAPID_API_KEY']
-    response = loc_client.request(request)
-    output = JSON.parse(response.read_body, symbolize_names: true)
-    #  @location = output[:area][:name]
-    render json: output
+  def create_clim_query
+    url = URI('WEATHER_URL') || URI('https://community-open-weather-map.p.rapidapi.com/weather?units=imperial&q=')
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true if url.scheme == 'https' && !http.started?
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(url)
+    request['X-RapidAPI-Host'] = 'WEATHER_HOST'
+    request['X-RapidAPI-Key'] = 'X_RAPIDAPI_KEY'
+    response = http.request(request)
+    clim_output = JSON.parse(response.read_body, symbolize_names: true)
+    render json: clim_output
+    run create_news_query, clim_output:
+  end
+
+  def create_news_query
+    url = URI('NEWS_URL') || 'https://latest-breaking-news-live.p.rapidapi.com/latest-breaking-news'
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true if url.scheme == 'https' && !http.started?
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(url)
+    request['X-RapidAPI-Host'] = 'NEWS_HOST'
+    request['X-RapidAPI-Key'] = 'X_RAPIDAPI_KEY'
+    response = http.request(request)
+    news_output = JSON.parse(response.read_body, symbolize_names: true)
+    render json: news_output
   end
 end
